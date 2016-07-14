29ac692b100890c4a38b8ae3670fce8fd8fe34ec
Hendrixer <willscottmoss@gmail.com>
2016-07-13 20:17:02 -0700

    chore

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Main } from './containers';
 
 @Component({
   selector: 'app',
+  directives: [
+    Main
+  ],
   template: `
     <div>
-      <h3>
-        Yo, world!
-      </h3>
+      <main-container></main-container>
     </div>
   `
 })
