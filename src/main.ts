c4b4e8225db3e8bad23ef8f4c9d436d973b6ae83
Hendrixer <willscottmoss@gmail.com>
2016-07-14 01:47:23 -0700

    chore

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, FORM_PROVIDERS } from '@angular/forms';
+import { provideRouter } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { App, providers } from './app';
+import { App, providers, routes } from './app';
 
 bootstrap(App, [
   ...FORM_PROVIDERS,
   ...HTTP_PROVIDERS,
   disableDeprecatedForms(),
+  provideRouter(routes),
+  { provide: LocationStrategy, useClass: HashLocationStrategy },
   ...providers
 ]);
