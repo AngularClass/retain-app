bd6207f49692b0cc831f21478459449ffb22e560
Hendrixer <willscottmoss@gmail.com>
2016-07-13 20:51:13 -0700

    chore

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AppBar } from '../ui/app-bar';
 
 @Component({
   selector: 'main-container',
+  directives: [
+    AppBar
+  ],
   template: `
-    <div>
+    <div class="main-container">
+      <app-bar></app-bar>
       <main class="main">
         content will go here
       </main>
