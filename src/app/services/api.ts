ad2b66ed44c402d74abe6015b3c0b8d214772e6b
Hendrixer <willscottmoss@gmail.com>
2016-07-14 00:43:16 -0700

    chore

---
+++ src/app/services/api.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class ApiService {
+  headers: Headers = new Headers({
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  });
+
+  api_url: string = 'http://localhost:3500';
+
+  constructor(private http: Http) {}
+
+  private getJson(response: Response) {
+    return response.json();
+  }
+
+  private checkForError(response: Response): Response {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText)
+      error['response'] = response;
+      console.error(error);
+      throw error;
+    }
+  }
+
+  get(path: string): Observable<any> {
+    return this.http.get(`${this.api_url}${path}`, { headers: this.headers })
+    .map(this.checkForError)
+    .catch(err => Observable.throw(err))
+    .map(this.getJson)
+  }
+
+  post(path: string, body): Observable<any> {
+    return this.http.post(
+      `${this.api_url}${path}`,
+      JSON.stringify(body),
+      { headers: this.headers }
+    )
+    .map(this.checkForError)
+    .catch(err => Observable.throw(err))
+    .map(this.getJson)
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http.delete(
+      `${this.api_url}${path}`,
+      { headers: this.headers }
+    )
+    .map(this.checkForError)
+    .catch(err => Observable.throw(err))
+    .map(this.getJson)
+  }
+}
