73e21d15f8d24b9be123990a300669cd23660d62
Hendrixer <willscottmoss@gmail.com>
2016-07-13 19:41:41 -0700

    init

---
+++ src/vendor.ts
@@ -0,0 +1,15 @@
+// Vendors
+
+// Angular 2
+import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS 5
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import 'rxjs/add/observable/throw';
+import 'rxjs/Rx';
