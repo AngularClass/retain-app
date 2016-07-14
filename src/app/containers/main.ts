cbe2684d1938383c6e1c21e83d5af43abc50eec6
Hendrixer <willscottmoss@gmail.com>
2016-07-13 20:17:02 -0700

    chore

---
+++ src/app/containers/main.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  template: `
+    <div>
+      <main class="main">
+        content will go here
+      </main>
+    </div>
+  `
+})
+export class Main {}
