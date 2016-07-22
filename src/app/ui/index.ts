48dda2ad78e343c2b08787ca408bc9c5c0f1bbcd
Hendrixer <willscottmoss@gmail.com>
2016-07-22 14:29:22 -0700

    chore

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -2,4 +2,3 @@ export { AppBar } from './app-bar';
 export { NoteCard } from './note-card';
 export { NoteCreator } from './note-creator';
 export { ColorPicker } from './color-picker';
-export { NoteCard } from './note-card';
