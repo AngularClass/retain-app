46df558dea36dc4e78b08d35cd94ac6cc83a5d9d
Hendrixer <willscottmoss@gmail.com>
2016-07-28 09:15:39 -0700

    chore

--- spec.entry.ts
+++ spec.entry.ts
@@ -13,12 +13,12 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import * as testing from '@angular/core/testing';
-import * as browser from '@angular/platform-browser-dynamic/testing';
+import { setBaseTestProviders } from '@angular/core/testing';
+import { TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS } from '@angular/platform-browser-dynamic/testing';
 
-testing.setBaseTestProviders(
-  browser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-  browser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
+setBaseTestProviders(
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
 );
 
 Object.assign(global, testing);
