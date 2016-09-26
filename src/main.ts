8e7795520f1e0f4b751a9e878ec04086eedd0582
Scott Moss <willscottmoss@gmail.com>
2016-09-25 21:36:20 -0700

    chore(release changes)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-
 import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { App } from './app'
 import { Main, Notes } from './app/containers'
-import { AppBar, NoteCard } from './app/ui'
+import { AppBar, NoteCard, NoteCreator } from './app/ui'
 
 @NgModule({
   declarations: [
@@ -13,7 +12,8 @@ import { AppBar, NoteCard } from './app/ui'
     Main,
     AppBar,
     NoteCard,
-    Notes
+    Notes,
+    NoteCreator
   ],
   imports: [BrowserModule, FormsModule],
   bootstrap: [App]
@@ -21,4 +21,3 @@ import { AppBar, NoteCard } from './app/ui'
 export class AppModule {}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
->>>>>>> module-7
