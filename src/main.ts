17890ddee13fab7c222fa52f668a5f2af170df03
Scott Moss <willscottmoss@gmail.com>
2016-09-25 21:20:17 -0700

    chore(release)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { App } from './app';
+import { NgModule }      from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { App } from './app'
+import { Main, Notes } from './app/containers'
+import { AppBar, NoteCard } from './app/ui'
 
-bootstrap(App);
+@NgModule({
+  declarations: [
+    App,
+    Main,
+    AppBar,
+    NoteCard,
+    Notes
+  ],
+  imports: [BrowserModule],
+  bootstrap: [App]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
