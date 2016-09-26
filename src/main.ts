8ca2ff28668f1e3047e332a8168bd27d63551333
Scott Moss <willscottmoss@gmail.com>
2016-09-26 00:38:46 -0700

    chore

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
-import { provideRouter } from '@angular/router';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { App, providers, routes } from './app';
-
-bootstrap(App, [
-  ...HTTP_PROVIDERS,
-  disableDeprecatedForms(),
-  provideForms(),
-  provideRouter(routes),
-  { provide: LocationStrategy, useClass: HashLocationStrategy },
-  ...providers
-]);
-=======
 import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { App } from './app'
-import { Main, Notes } from './app/containers'
+import { App, routes } from './app'
+import { Main, Notes, About } from './app/containers'
 import { 
   AppBar,
   NoteCard,
@@ -41,16 +24,21 @@ import {
     NoteCard,
     Notes,
     NoteCreator,
-    ColorPicker
+    ColorPicker,
+    About
   ],
   providers: [
     NoteService,
     ApiService
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routes
+  ],
   bootstrap: [App]
 })
 export class AppModule {}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
->>>>>>> module-12
