28776d4f27098a7f1c3d5b440ca07ac43b97d7bf
lestote <lestote@hotmail.fr>
2016-12-20 11:30:06 +0100

    adding new files - ui components building up

---
+++ src/app/containers/main.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  template: `
+    <div>
+      <main class="main">
+        Content here...
+      </main>
+    </div>
+  `
+})
+export class Main {};
---
+++ src/app/ui/app-bar.ts
@@ -0,0 +1,11 @@
+export { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  styles: [`
+      .navbar {
+        backroung-color: blue;
+      }
+    `]
+})
+export class AppBar{};
